5678f94e9f50579ae630438ab7941919002f4a9b
MarOUAFIK <marwane.ouafik@outlook.fr>
2024-07-16 02:47:33 +0100

    Added EF and Database

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data;
+
+public class HotelListingDbContext : DbContext {
+    public HotelListingDbContext( DbContextOptions options ) : base( options ) {
+
+    }
+
+    protected override void OnModelCreating( ModelBuilder modelBuilder ) {
+        base.OnModelCreating( modelBuilder );
+
+        modelBuilder.Entity<Country>().HasData(
+            new Country { Id = 1,Name = "Jamaica",ShortName = "JM" },
+			new Country { Id = 2,Name = "Bahamas",ShortName = "BS" },
+			new Country { Id = 3,Name = "Cayman Island",ShortName = "CI" }
+		);
+
+        modelBuilder.Entity<Hotel>().HasData(
+           new Hotel { Id = 1,Name = "Sandals Resort & Spa",Address = "Negril",Rating = 4.3, CountryId = 1 },
+           new Hotel { Id = 2,Name = "Comfort Suites",Address = "George Town",Rating = 4.3, CountryId = 3 },
+           new Hotel { Id = 3,Name = "Grand Paldium",Address = "Nassua",Rating = 4.3, CountryId = 2 }
+          
+       );
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    public DbSet<Hotel> Hotels { get; set; }
+}
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,3 +1,7 @@
+using HotelListing.API.Data;
+
+using Microsoft.EntityFrameworkCore;
+
 using Serilog;
 
 namespace HotelListing.API;
@@ -8,6 +12,10 @@ public class Program {
 
         // Add services to the container.
 
+        var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString") ?? "";
+        builder.Services.AddDbContext<HotelListingDbContext>( options => {
+            options.UseSqlServer( connectionString );
+        } );
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
